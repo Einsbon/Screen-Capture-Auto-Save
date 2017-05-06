fe20864fff4af4a08c08b1b4d1ae2311d5140261
Einsbon Kim <einsbon@gmail.com>
2017-05-07 02:34:37 +0900

    초기 커밋.

---
+++ Screen Capture Auto Save/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Screen_Capture_Auto_Save
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTray = new System.Windows.Forms.Button();
+            this.btnOpenPath = new System.Windows.Forms.Button();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnManual = new System.Windows.Forms.Button();
+            this.lblVer = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblInform = new System.Windows.Forms.Label();
+            this.checkMulti = new System.Windows.Forms.CheckBox();
+            this.btnLang = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnTray
+            // 
+            this.btnTray.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnTray.Location = new System.Drawing.Point(12, 143);
+            this.btnTray.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnTray.Name = "btnTray";
+            this.btnTray.Size = new System.Drawing.Size(201, 28);
+            this.btnTray.TabIndex = 1;
+            this.btnTray.Text = "트레이 아이콘에 표시";
+            this.btnTray.UseVisualStyleBackColor = true;
+            this.btnTray.Click += new System.EventHandler(this.btnTray_Click);
+            // 
+            // btnOpenPath
+            // 
+            this.btnOpenPath.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOpenPath.Location = new System.Drawing.Point(12, 106);
+            this.btnOpenPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnOpenPath.Name = "btnOpenPath";
+            this.btnOpenPath.Size = new System.Drawing.Size(201, 28);
+            this.btnOpenPath.TabIndex = 2;
+            this.btnOpenPath.Text = "저장 경로 열기";
+            this.btnOpenPath.UseVisualStyleBackColor = true;
+            this.btnOpenPath.Click += new System.EventHandler(this.btnOpenPath_Click);
+            // 
+            // btnPath
+            // 
+            this.btnPath.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnPath.Location = new System.Drawing.Point(12, 69);
+            this.btnPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(201, 28);
+            this.btnPath.TabIndex = 3;
+            this.btnPath.Text = "저장 경로 설정";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnManual
+            // 
+            this.btnManual.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnManual.Location = new System.Drawing.Point(12, 180);
+            this.btnManual.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnManual.Name = "btnManual";
+            this.btnManual.Size = new System.Drawing.Size(86, 28);
+            this.btnManual.TabIndex = 5;
+            this.btnManual.Text = "설명서";
+            this.btnManual.UseVisualStyleBackColor = true;
+            this.btnManual.Click += new System.EventHandler(this.btnManual_Click);
+            // 
+            // lblVer
+            // 
+            this.lblVer.AutoSize = true;
+            this.lblVer.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblVer.Location = new System.Drawing.Point(638, 185);
+            this.lblVer.Name = "lblVer";
+            this.lblVer.Size = new System.Drawing.Size(72, 18);
+            this.lblVer.TabIndex = 6;
+            this.lblVer.Text = "버전 0.6";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPath.Location = new System.Drawing.Point(219, 74);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(0, 18);
+            this.lblPath.TabIndex = 7;
+            // 
+            // lblInform
+            // 
+            this.lblInform.AutoSize = true;
+            this.lblInform.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblInform.Location = new System.Drawing.Point(26, 19);
+            this.lblInform.Name = "lblInform";
+            this.lblInform.Size = new System.Drawing.Size(0, 18);
+            this.lblInform.TabIndex = 8;
+            // 
+            // checkMulti
+            // 
+            this.checkMulti.AutoSize = true;
+            this.checkMulti.Location = new System.Drawing.Point(306, 184);
+            this.checkMulti.Name = "checkMulti";
+            this.checkMulti.Size = new System.Drawing.Size(195, 22);
+            this.checkMulti.TabIndex = 9;
+            this.checkMulti.Text = "여러 모니터 모두 캡쳐";
+            this.checkMulti.UseVisualStyleBackColor = true;
+            // 
+            // btnLang
+            // 
+            this.btnLang.Location = new System.Drawing.Point(687, 14);
+            this.btnLang.Name = "btnLang";
+            this.btnLang.Size = new System.Drawing.Size(325, 28);
+            this.btnLang.TabIndex = 10;
+            this.btnLang.Text = "Change Language ( English / Korean )";
+            this.btnLang.UseVisualStyleBackColor = true;
+            this.btnLang.Click += new System.EventHandler(this.btnLang_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(638, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 54);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "개발자 블로그 :\r\n이메일 주소 :\r\nGitHub 주소 : ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(772, 111);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(240, 18);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://blog.naver.com/einsbon";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(772, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 18);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "einsbon@gmail.com";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(772, 147);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(216, 18);
+            this.linkLabel2.TabIndex = 14;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/Einsbon";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(306, 109);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(194, 22);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PrintScreen 으로 캡쳐";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(306, 137);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(258, 22);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Shift + PrintScreen 으로 캡쳐";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 223);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLang);
+            this.Controls.Add(this.checkMulti);
+            this.Controls.Add(this.lblInform);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.lblVer);
+            this.Controls.Add(this.btnManual);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.btnOpenPath);
+            this.Controls.Add(this.btnTray);
+            this.Font = new System.Drawing.Font("D2Coding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "스크린샷 자동 저장";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTray;
+        private System.Windows.Forms.Button btnOpenPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnManual;
+        private System.Windows.Forms.Label lblVer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblInform;
+        private System.Windows.Forms.CheckBox checkMulti;
+        private System.Windows.Forms.Button btnLang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Screen Capture Auto Save/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Utilities;
+
+namespace Screen_Capture_Auto_Save
+{
+    public partial class Form1 : Form
+    {
+        bool korean = Properties.Settings.Default.boolKorean;
+        string filePath = Properties.Settings.Default.strPath;
+        bool multiScreen = Properties.Settings.Default.boolMultiScr;
+        bool shift = Properties.Settings.Default.boolShift;
+
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        globalKeyboardHook gkhShift = new globalKeyboardHook();
+
+        public Form1()
+        {
+            InitializeComponent();
+            gkh.HookedKeys.Add(Keys.PrintScreen);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+
+            gkhShift.HookedKeys.Add(Keys.RShiftKey);
+            gkhShift.HookedKeys.Add(Keys.LShiftKey);
+            gkhShift.KeyDown += new KeyEventHandler(gkhP_KeyDown);
+            gkhShift.KeyUp += new KeyEventHandler(gkhP_KeyUp);
+
+            radioButton2.Checked = Properties.Settings.Default.boolShift;
+            radioButton1.Checked = !Properties.Settings.Default.boolShift;
+
+            korean = Properties.Settings.Default.boolKorean;
+            checkMulti.Checked = multiScreen;
+            lblPath.Text = "저장 경로 : " + filePath;
+
+            if (shift)
+            {
+                if (korean)
+                {
+                    lblInform.Text = "Shift + PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                }
+                else
+                {
+                    lblInform.Text = "Press 'Shift + PrintScreen' buttons to Capture and Save";
+                }
+            }
+            else
+            {
+                if (korean)
+                {
+                    lblInform.Text = "PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                }
+                else
+                {
+                    lblInform.Text = "Press PrintScreen buttons to Capture and Save";
+                }
+            }
+
+            //언어 설정 불러오기
+            if (!korean)
+            {
+                btnManual.Text = "Manual";
+                btnOpenPath.Text = "Open save location";
+                btnPath.Text = "Change save location";
+                btnTray.Text = "Show on tray icon";
+                checkMulti.Text = "Capture Multi Screen";
+                lblInform.Text = "Press 'left Shift + PrintScreen' buttons to Capture and Save";
+                lblVer.Text = "Version 0.6";
+                lblPath.Text = "Save location : " + filePath;
+                label1.Text = "Developer's blog :\r\nE-mail address:\r\nGitHub address :";
+                if (shift)
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "Shift + PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press 'Shift + PrintScreen' buttons to Capture and Save";
+                    }
+                }
+                else
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press PrintScreen buttons to Capture and Save";
+                    }
+                }
+            }
+        }
+
+        private void gkhP_KeyUp(object sender, KeyEventArgs e)
+        {
+            shiftPressed = false;
+        }
+        bool shiftPressed = false;
+
+        private void gkhP_KeyDown(object sender, KeyEventArgs e)
+        {
+            shiftPressed = true;
+        }
+
+        class Screencapture
+        {
+            //캡쳐
+            public static void Capture(string path1, bool multiScr)
+            {
+                if (!multiScr)
+                {
+                    Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    Graphics graphics = Graphics.FromImage(bitmap);
+                    graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+                    string fileName = path1 + "\\" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+                    string finalName = fileName;
+                    if (System.IO.File.Exists(fileName + ".png"))
+                    {
+                        int count = 1;
+                        while (System.IO.File.Exists(finalName + ".png"))
+                        {
+                            finalName = fileName + "-" + count++;
+                        }
+                    }
+                    bitmap.Save(finalName + ".png", ImageFormat.Png);
+                    graphics.Dispose();//메모리 정리
+                    bitmap.Dispose();//메모리 정리
+                }
+                else
+                {
+                    Screen[] screens;
+                    screens = Screen.AllScreens;
+                    int screensConunt = screens.Length;
+
+                    for (int scrNum = 0; scrNum < screensConunt; scrNum++)
+                    {
+                        Bitmap bitmap = new Bitmap(screens[scrNum].Bounds.Width, screens[scrNum].Bounds.Height);
+                        Graphics graphics = Graphics.FromImage(bitmap);
+                        graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+                        string fileName = path1 + "\\" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")+ "_monitorNo" + scrNum;
+                        string finalName = fileName;
+                        if (System.IO.File.Exists(fileName + ".png"))
+                        {
+                            int count = 1;
+                            while (System.IO.File.Exists(finalName + ".png"))
+                            {
+                                finalName = fileName + "-" + count++;
+                            }
+                        }
+                        bitmap.Save(finalName + ".png", ImageFormat.Png);
+                        graphics.Dispose();//메모리 정리
+                        bitmap.Dispose();//메모리 정리
+                    }
+                }
+            }
+        }
+
+        //버튼: 트레이 아이콘에 표시
+        private void btnTray_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true; // tray icon 표시
+            this.Hide();//이 창 숨기기
+            this.ShowInTaskbar = false;//작업 표시줄에서도 안보이기
+        }
+        //버튼: 저장 경로 설정
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = this.folderBrowserDialog1.SelectedPath;//filePath에 경로 저장
+            }
+            lblPath.Text = "저장 경로 : " + filePath; ;
+        }
+        //버튼: 저장 경로 열기
+        private void btnOpenPath_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(filePath);
+        }
+        //프린트스크린 키 이벤트
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Screencapture.Capture(filePath, checkMulti.Checked);
+            }
+            else
+            {
+                if (shiftPressed)
+                {
+                    Screencapture.Capture(filePath, checkMulti.Checked);
+                }
+            }
+        }
+
+        //버튼: 설명서
+        private void btnManual_Click(object sender, EventArgs e)
+        {
+            if (korean)
+            {
+                MessageBox.Show("스크린 캡쳐 후 자동으로 저장해주는 프로그램입니다.\r\n\r\n" +
+                "왼쪽 Shift + PrintScreen 버튼을 누르면 설정한 경로 속에 자동 저장 됩니다.\r\n\r\n" +
+                "이 프로그램은 켜져있는 동안 작동하며 '창 숨기기' 버튼을 누르면 창이 사라집니다.\r\n\r\n" +
+                "트레이 아이콘을 클릭 하면 창이 다시 나타납니다.\r\n\r\n " +
+                "혹시 오류나 개선해야될 점이 있으면 개발자 이메일로 보내주시면 감사합니다.^_^");
+            }
+            else
+            {
+                MessageBox.Show("press 'P + PrintScreen' button to capture and save.\r\n\r\n" +
+                    "'Show on tray icon' button will hide this window and show Tray Icon\r\n\r\n" +
+                    "click the tray icon to show hided window.\r\n\r\n" +
+                    "This program olny works while not closed.\r\n\r\n" +
+                    "If you see trouble on this program, please send an e-mail to me");
+            }
+        }
+        //트레이 버튼: 트레이 아이콘 더블클릭 시
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        //언어 설정
+        private void btnLang_Click(object sender, EventArgs e)
+        {
+            korean = !korean;
+            if (!korean)
+            {
+                btnManual.Text = "Manual";
+                btnOpenPath.Text = "Open save location";
+                btnPath.Text = "Change save location";
+                btnTray.Text = "Show on tray icon";
+                checkMulti.Text = "Capture Multi Screen";
+                lblInform.Text = "Press 'left Shift + PrintScreen' buttons to Capture and Save";
+                lblVer.Text = "Version 0.6";
+                lblPath.Text = "Save location : " + filePath;
+                label1.Text = "Developer's blog :\r\nE-mail address:\r\nGitHub address :";
+                radioButton1.Text = "PrintScreen to capture";
+                radioButton2.Text = "Shift + PrintScreen to capture";
+                if (radioButton2.Checked)
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "Shift + PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press 'Shift + PrintScreen' buttons to Capture and Save";
+                    }
+                }
+                else
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press PrintScreen buttons to Capture and Save";
+                    }
+                }
+                label1.Left = 614;
+            }
+            else
+            {
+                btnManual.Text = "설명서";
+                btnOpenPath.Text = "저장 폴더 열기";
+                btnPath.Text = "저장 경로 설정";
+                btnTray.Text = "트레이 아이콘에 표시";
+                checkMulti.Text = "여러 모니터 모두 캡쳐";
+                lblInform.Text = "왼쪽 Shift + PrintScreen 버튼을 누르면 화면이 저장 됩니다.";
+                lblVer.Text = "버전 0.6";
+                lblPath.Text = "저장 경로 : " + filePath;
+                label1.Text = "개발자 블로그 :\r\n이메일 주소 :\r\nGitHub 주소 :";
+                radioButton1.Text = "PrintScreen 으로 캡쳐";
+                radioButton2.Text = "Shift + PrintScreen 으로 캡쳐";
+                if (radioButton2.Checked)
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "Shift + PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press 'Shift + PrintScreen' buttons to Capture and Save";
+                    }
+                }
+                else
+                {
+                    if (korean)
+                    {
+                        lblInform.Text = "PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                    }
+                    else
+                    {
+                        lblInform.Text = "Press PrintScreen buttons to Capture and Save";
+                    }
+                }
+                label1.Left = 638;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://blog.naver.com/einsbon");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Einsbon");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.boolKorean = korean;
+            Properties.Settings.Default.boolMultiScr = checkMulti.Checked;
+            Properties.Settings.Default.strPath = filePath;
+            Properties.Settings.Default.boolShift = radioButton2.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                if (korean)
+                {
+                    lblInform.Text = "Shift + PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                }
+                else
+                {
+                    lblInform.Text = "Press 'Shift + PrintScreen' buttons to Capture and Save";
+                }
+            }
+            else
+            {
+                if (korean)
+                {
+                    lblInform.Text = "PrintScreen 버튼을 누르면 화면이 저장됩니다.";
+                }
+                else
+                {
+                    lblInform.Text = "Press PrintScreen buttons to Capture and Save";
+                }
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.Show();
+            this.ShowInTaskbar = true;
+        }
+    }
+}
